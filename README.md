05228becc8c5127b0555bef8a800140461212016
André Peixoto <andrepeixoto@gmail.com>
2023-02-14 18:29:18 -0300

    Ajustes de layout com Tailwind

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next
 ## Fase 1
 
 Importei algumas imagens do site Pexel num carrossel e estou testando algumas funções do componente Image do Next e a biblioteca Sharp do Node.
+
+# Fase 2
+
+Fiz alguns ajustes no layout com Tailwind e na próxima fase vou fazer testes com animações com Framer Motion.
--- app/page.tsx
+++ app/page.tsx
@@ -21,57 +21,49 @@ export default function App() {
   }
 
   return (
-    <div className="flex-row justify-center align-middle min-h-screen w-full bg-gray-300">
-      <div
-        id="left"
-        className="absolute left-0 z-10 top-[15rem] cursor-pointer"
-        onClick={prevSlide}
-      >
-        <svg
-          className="w-[3rem]"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
+    <>
+      <div className="fixed flex flex-row min-h-screen w-full px-5">
+        <div className="flex w-1/2 cursor-pointer" onClick={prevSlide}>
+          <svg
+            className="w-[4rem]"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M12 2.5a9.5 9.5 0 1 0 9.5 9.5A9.509 9.509 0 0 0 12 2.5Zm1.71 11.79a1.008 1.008 0 0 1 0 1.42 1.014 1.014 0 0 1-1.42 0l-3-3a1.008 1.008 0 0 1 0-1.42l3-3a1 1 0 0 1 1.42 1.42L11.41 12Z" />
+          </svg>
+        </div>
+
+        <div
+          className="flex justify-end w-1/2 cursor-pointer"
+          onClick={nextSlide}
         >
-          <path
-            data-name="chevron-circle-left-Filled"
-            d="M12 2.5a9.5 9.5 0 1 0 9.5 9.5A9.509 9.509 0 0 0 12 2.5Zm1.71 11.79a1.008 1.008 0 0 1 0 1.42 1.014 1.014 0 0 1-1.42 0l-3-3a1.008 1.008 0 0 1 0-1.42l3-3a1 1 0 0 1 1.42 1.42L11.41 12Z"
-          />
-        </svg>
+          <svg
+            className="w-[4rem]"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M12 2.5a9.5 9.5 0 1 0 9.5 9.5A9.509 9.509 0 0 0 12 2.5Zm2.71 10.21-3 3a1.014 1.014 0 0 1-1.42 0 1.008 1.008 0 0 1 0-1.42l2.3-2.29-2.3-2.29a1 1 0 0 1 1.42-1.42l3 3a1.008 1.008 0 0 1 0 1.42Z" />
+          </svg>
+        </div>
       </div>
-      <div
-        id="right"
-        className="absolute right-0 z-10 top-[15rem] cursor-pointer"
-        onClick={nextSlide}
-      >
-        <svg
-          className="w-[3rem]"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            data-name="chevron-circle-right-Filled"
-            d="M12 2.5a9.5 9.5 0 1 0 9.5 9.5A9.509 9.509 0 0 0 12 2.5Zm2.71 10.21-3 3a1.014 1.014 0 0 1-1.42 0 1.008 1.008 0 0 1 0-1.42l2.3-2.29-2.3-2.29a1 1 0 0 1 1.42-1.42l3 3a1.008 1.008 0 0 1 0 1.42Z"
-          />
-        </svg>
+
+      <div className="flex min-h-screen w-full z-10">
+        {images.map(
+          (image, index) =>
+            current === index && (
+              <div key={index} className="grow bg-red-100">
+                <Image
+                  width={600}
+                  height={400}
+                  src={image}
+                  alt="images"
+                  className="w-full"
+                  priority={false}
+                />
+              </div>
+            )
+        )}
       </div>
-      {images.map(
-        (image, index) =>
-          current === index && (
-            <div
-              key={index}
-              className="flex w-full justify-center align-middle pt-[3rem]"
-            >
-              <Image
-                width={600}
-                height={400}
-                src={image}
-                alt="images"
-                className="rounded-lg"
-                priority={false}
-              />
-            </div>
-          )
-      )}
-    </div>
+    </>
   );
 }
